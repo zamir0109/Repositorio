d6ecd6835587e2faeb7907b90fa73608bfe09f46
anzapemo <ansapemo@hotmail.com>
2014-03-03 13:51:16 -0500

    SAdasdas

---
+++ App/src/com/monroy/prueba/OtraClase.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.monroy.prueba;
+
+/**
+ *
+ * @author Zamir
+ */
+public class OtraClase {
+    
+    public OtraClase(){
+        
+    }
+    
+}
